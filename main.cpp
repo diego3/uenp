342b1cf0701231299a2aa150bba3bf5f80e15196
Diego Rosa dos Santos <www.diegosantos.com.br@gmail.com>
2015-11-26 15:56:13 -0200

    - first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
